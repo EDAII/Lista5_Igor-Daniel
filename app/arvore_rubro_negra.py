fc3cdef6cdf41b25cc8dc25c3fd5dadf341e6269
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-06-01 12:20:42 -0300

    Adding more methods
    Co-authored-by: zero101010 <igotaraujo97@gmail.com>

--- app/arvore_rubro_negra.py
+++ app/arvore_rubro_negra.py
@@ -104,3 +104,129 @@ class RedBlackTree:
                     x = self.root
         x.color = 0
 
+    def __rb_transplant(self, u, v):
+        if u.parent is None:
+            self.root = v
+        elif u == u.parent.left:
+            u.parent.left = v
+        else:
+            u.parent.right = v
+        v.parent = u.parent
+
+    def __delete_node_helper(self, node, key):
+        # acha o nó correspondente a chave
+        z = self.TNULL
+        while node != self.TNULL:
+            if node.data == key:
+                z = node
+
+            if node.data <= key:
+                node = node.right
+            else:
+                node = node.left
+
+        if z == self.TNULL:
+            print("Couldn't find key in the tree")
+            return
+
+        y = z
+        y_original_color = y.color
+        if z.left == self.TNULL:
+            x = z.right
+            self.__rb_transplant(z, z.right)
+        elif (z.right == self.TNULL):
+            x = z.left
+            self.__rb_transplant(z, z.left)
+        else:
+            y = self.minimum(z.right)
+            y_original_color = y.color
+            x = y.right
+            if y.parent == z:
+                x.parent = y
+            else:
+                self.__rb_transplant(y, y.right)
+                y.right = z.right
+                y.right.parent = y
+
+            self.__rb_transplant(z, y)
+            y.left = z.left
+            y.left.parent = y
+            y.color = z.color
+        if y_original_color == 0:
+            self.__fix_delete(x)
+
+    # fixa a árvore
+    def __fix_insert(self, k):
+        while k.parent.color == 1:
+            if k.parent == k.parent.parent.right:
+                u = k.parent.parent.left  # uncle
+                if u.color == 1:
+                    # case 3.1
+                    u.color = 0
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.left:
+                        # case 3.2.2
+                        k = k.parent
+                        self.right_rotate(k)
+                    # case 3.2.1
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    self.left_rotate(k.parent.parent)
+            else:
+                u = k.parent.parent.right  # uncle
+
+                if u.color == 1:
+                    # mirror case 3.1
+                    u.color = 0
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.right:
+                        # mirror case 3.2.2
+                        k = k.parent
+                        self.left_rotate(k)
+                    # mirror case 3.2.1
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    self.right_rotate(k.parent.parent)
+            if k == self.root:
+                break
+        self.root.color = 0
+
+        # rotate left at node x
+        def left_rotate(self, x):
+            y = x.right
+            x.right = y.left
+            if y.left != self.TNULL:
+                y.left.parent = x
+
+            y.parent = x.parent
+            if x.parent == None:
+                self.root = y
+            elif x == x.parent.left:
+                x.parent.left = y
+            else:
+                x.parent.right = y
+            y.left = x
+            x.parent = y
+
+        # rotate right at node x
+        def right_rotate(self, x):
+            y = x.left
+            x.left = y.right
+            if y.right != self.TNULL:
+                y.right.parent = x
+
+            y.parent = x.parent
+            if x.parent == None:
+                self.root = y
+            elif x == x.parent.right:
+                x.parent.right = y
+            else:
+                x.parent.left = y
+            y.right = x
+            x.parent = y
