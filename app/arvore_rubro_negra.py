c2980ebfe9ac231aff310f9c1d65ec3eab28f12f
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-06-01 09:36:28 -0300

    Adding fix tree

--- app/arvore_rubro_negra.py
+++ app/arvore_rubro_negra.py
@@ -44,3 +44,63 @@ class RedBlackTree:
         if key < node.data:
             return self.__search_tree_helper(node.left, key)
         return self.__search_tree_helper(node.right, key)
+
+    # fixa a arvore rb modificada pela operação deletada
+    def __fix_delete(self, x):
+        while x != self.root and x.color == 0:
+            if x == x.parent.left:
+                s = x.parent.right
+                if s.color == 1:
+                    # case 3.1
+                    s.color = 0
+                    x.parent.color = 1
+                    self.left_rotate(x.parent)
+                    s = x.parent.right
+
+                if s.left.color == 0 and s.right.color == 0:
+                    # case 3.2
+                    s.color = 1
+                    x = x.parent
+                else:
+                    if s.right.color == 0:
+                        # case 3.3
+                        s.left.color = 0
+                        s.color = 1
+                        self.right_rotate(s)
+                        s = x.parent.right
+
+                    # case 3.4
+                    s.color = x.parent.color
+                    x.parent.color = 0
+                    s.right.color = 0
+                    self.left_rotate(x.parent)
+                    x = self.root
+            else:
+                s = x.parent.left
+                if s.color == 1:
+                    # case 3.1
+                    s.color = 0
+                    x.parent.color = 1
+                    self.right_rotate(x.parent)
+                    s = x.parent.left
+
+                if s.right.color == 0 and s.right.color == 0:
+                    # case 3.2
+                    s.color = 1
+                    x = x.parent
+                else:
+                    if s.left.color == 0:
+                        # case 3.3
+                        s.right.color = 0
+                        s.color = 1
+                        self.left_rotate(s)
+                        s = x.parent.left
+
+                        # case 3.4
+                    s.color = x.parent.color
+                    x.parent.color = 0
+                    s.left.color = 0
+                    self.right_rotate(x.parent)
+                    x = self.root
+        x.color = 0
+
