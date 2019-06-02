2baa63166d523e1ae32e7bde6981f763e248d4be
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-06-02 18:11:52 -0300

    Finishing red black tree
    Co-authored-by: zero101010 <igotaraujo97@gmail.com>

--- app/arvore_rubro_negra.py
+++ app/arvore_rubro_negra.py
@@ -197,36 +197,172 @@ class RedBlackTree:
                 break
         self.root.color = 0
 
-        # rotate left at node x
-        def left_rotate(self, x):
-            y = x.right
-            x.right = y.left
-            if y.left != self.TNULL:
-                y.left.parent = x
-
-            y.parent = x.parent
-            if x.parent == None:
-                self.root = y
-            elif x == x.parent.left:
-                x.parent.left = y
+    def __print_helper(self, node, indent, last):
+        # printa a arvore na tela
+        if node != self.TNULL:
+            sys.stdout.write(indent)
+            if last:
+                sys.stdout.write("R----")
+                indent += "     "
             else:
-                x.parent.right = y
-            y.left = x
-            x.parent = y
+                sys.stdout.write("L----")
+                indent += "|    "
 
-        # rotate right at node x
-        def right_rotate(self, x):
-            y = x.left
-            x.left = y.right
-            if y.right != self.TNULL:
-                y.right.parent = x
+            s_color = "RED" if node.color == 1 else "BLACK"
+            print (str(node.data) + "(" + s_color + ")")
+            self.__print_helper(node.left, indent, False)
+            self.__print_helper(node.right, indent, True)
 
-            y.parent = x.parent
-            if x.parent == None:
-                self.root = y
-            elif x == x.parent.right:
-                x.parent.right = y
+    # Pre-Order traversal
+    # Node.Left Subtree.Right Subtree
+    def preorder(self):
+        self.__pre_order_helper(self.root)
+
+    # In-Order traversal
+    # left Subtree . Node . Right Subtree
+    def inorder(self):
+        self.__in_order_helper(self.root)
+
+    # Post-Order traversal
+    # Left Subtree . Right Subtree . Node
+    def postorder(self):
+        self.__post_order_helper(self.root)
+
+    # search the tree for the key k
+    # and return the corresponding node
+    def searchTree(self, k):
+        return self.__search_tree_helper(self.root, k)
+
+    # find the node with the minimum key
+    def minimum(self, node):
+        while node.left != self.TNULL:
+            node = node.left
+        return node
+
+    # find the node with the maximum key
+    def maximum(self, node):
+        while node.right != self.TNULL:
+            node = node.right
+        return node
+
+    # find the successor of a given node
+    def successor(self, x):
+        if x.right != self.TNULL:
+            return self.minimum(x.right)
+
+        y = x.parent
+        while y != self.TNULL and x == y.right:
+            x = y
+            y = y.parent
+        return y
+
+    # find the predecessor of a given node
+    def predecessor(self, x):
+        if (x.left != self.TNULL):
+            return self.maximum(x.left)
+
+        y = x.parent
+        while y != self.TNULL and x == y.left:
+            x = y
+            y = y.parent
+
+        return y
+
+    # rotate left at node x
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.TNULL:
+            y.left.parent = x
+
+        y.parent = x.parent
+        if x.parent == None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    # rotate right at node x
+    def right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right != self.TNULL:
+            y.right.parent = x
+
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
+
+    # insert the key to the tree in its appropriate position
+    # and fix the tree
+    def insert(self, key):
+        # Ordinary Binary Search Insertion
+        node = Node(key)
+        node.parent = None
+        node.data = key
+        node.left = self.TNULL
+        node.right = self.TNULL
+        node.color = 1
+
+        y = None
+        x = self.root
+
+        while x != self.TNULL:
+            y = x
+            if node.data < x.data:
+                x = x.left
             else:
-                x.parent.left = y
-            y.right = x
-            x.parent = y
+                x = x.right
+
+        # y is parent of x
+        node.parent = y
+        if y == None:
+            self.root = node
+        elif node.data < y.data:
+            y.left = node
+        else:
+            y.right = node
+
+        # if new node is a root node, simply return
+        if node.parent == None:
+            node.color = 0
+            return
+
+        # if the grandparent is None, simply return
+        if node.parent.parent == None:
+            return
+
+        self.__fix_insert(node)
+
+    def get_root(self):
+        return self.root
+
+    def delete_node(self, data):
+        self.__delete_node_helper(self.root, data)
+
+    # printa a estrutura na tela
+    def pretty_print(self):
+        self.__print_helper(self.root, "", True)
+
+
+if __name__ == "__main__":
+    bst = RedBlackTree()
+    bst.insert(8)
+    bst.insert(18)
+    bst.insert(5)
+    bst.insert(15)
+    bst.insert(17)
+    bst.insert(25)
+    bst.insert(40)
+    bst.insert(80)
+    # bst.delete_node(25)
+    bst.pretty_print()
